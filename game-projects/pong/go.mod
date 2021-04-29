421a310301c94b40e4884ee36096041320d6b047
Preslav Mihaylov <preslav@kingslanduniversity.com>
2021-04-29 08:41:34 +0300

    add the pong game project

---
+++ game-projects/pong/go.mod
@@ -0,0 +1,8 @@
+module pong
+
+go 1.16
+
+require (
+	github.com/gdamore/tcell v1.4.0 // indirect
+	github.com/mattn/go-runewidth v0.0.12 // indirect
+)
---
+++ game-projects/pong/main.go
@@ -0,0 +1,247 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/gdamore/tcell"
+)
+
+// Extra Challenges:
+// 1. Optimize screen rendering
+//    (remove screen.Clear and explicitly clear only the pixels you need to delete)
+// 2. Change the colors of objects in-game
+// 3. Randomize ball speed
+// 4. Don't game over on scored points.
+//    Instead, keep track of scores,
+//    reset the game after someone scores and "game over"
+//    once someone reaches score of 5.
+//    For this challenge, you'll have to run your game inside an
+//    explicit frame on the screen instead of always running it "full screen".
+//    This will enable you to get some free space to render the scores in the game.
+// 5. Add in-game bonuses
+// 6. Dynamically change ball direction based on where the ball hits the paddle
+
+const PaddleSymbol = 0x2588
+const BallSymbol = 0x25CF
+
+const PaddleHeight = 4
+const InitialBallVelocityRow = 1
+const InitialBallVelocityCol = 2
+
+type GameObject struct {
+	row, col, width, height int
+	velRow, velCol          int
+	symbol                  rune
+}
+
+var screen tcell.Screen
+var player1Paddle *GameObject
+var player2Paddle *GameObject
+var ball *GameObject
+var isGamePaused bool
+var debugLog string
+
+var gameObjects []*GameObject
+
+// This program just prints "Hello, World!".  Press ESC to exit.
+func main() {
+	InitScreen()
+	InitGameState()
+	inputChan := InitUserInput()
+
+	for !IsGameOver() {
+		HandleUserInput(ReadInput(inputChan))
+		UpdateState()
+		DrawState()
+
+		time.Sleep(75 * time.Millisecond)
+	}
+
+	screenWidth, screenHeight := screen.Size()
+	winner := GetWinner()
+	PrintStringCentered(screenHeight/2-1, screenWidth/2, "Game Over!")
+	PrintStringCentered(screenHeight/2, screenWidth/2, fmt.Sprintf("%s wins...", winner))
+	screen.Show()
+
+	time.Sleep(3 * time.Second)
+	screen.Fini()
+}
+
+func UpdateState() {
+	if isGamePaused {
+		return
+	}
+
+	for i := range gameObjects {
+		gameObjects[i].row += gameObjects[i].velRow
+		gameObjects[i].col += gameObjects[i].velCol
+	}
+
+	// debugLog = fmt.Sprintf("ball: row=%d, col=%d\npaddle 1: row=%d, col=%d\npaddle 2: row=%d, col=%d",
+	// 	ball.row, ball.col, player1Paddle.row, player1Paddle.col, player2Paddle.row, player2Paddle.col)
+	if CollidesWithWall(ball) {
+		ball.velRow = -ball.velRow
+	}
+
+	if CollidesWithPaddle(ball, player1Paddle) || CollidesWithPaddle(ball, player2Paddle) {
+		ball.velCol = -ball.velCol
+	}
+}
+
+func DrawState() {
+	if isGamePaused {
+		return
+	}
+
+	screen.Clear()
+	PrintString(0, 0, debugLog)
+	for _, obj := range gameObjects {
+		Print(obj.row, obj.col, obj.width, obj.height, obj.symbol)
+	}
+
+	screen.Show()
+}
+
+func CollidesWithWall(obj *GameObject) bool {
+	_, screenHeight := screen.Size()
+	return obj.row+obj.velRow < 0 || obj.row+obj.velRow >= screenHeight
+}
+
+func CollidesWithPaddle(ball *GameObject, paddle *GameObject) bool {
+	var collidesOnColumn bool
+	if ball.col < paddle.col {
+		collidesOnColumn = ball.col+ball.velCol >= paddle.col
+	} else {
+		collidesOnColumn = ball.col+ball.velCol <= paddle.col
+	}
+
+	return collidesOnColumn &&
+		ball.row >= paddle.row &&
+		ball.row < paddle.row+paddle.height
+}
+
+func IsGameOver() bool {
+	return GetWinner() != ""
+}
+
+func GetWinner() string {
+	screenWidth, _ := screen.Size()
+	if ball.col < 0 {
+		return "Player 1"
+	} else if ball.col >= screenWidth {
+		return "Player 2"
+	} else {
+		return ""
+	}
+}
+
+func InitScreen() {
+	var err error
+	screen, err = tcell.NewScreen()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+
+	if err := screen.Init(); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+
+	defStyle := tcell.StyleDefault.
+		Background(tcell.ColorBlack).
+		Foreground(tcell.ColorWhite)
+	screen.SetStyle(defStyle)
+}
+
+func InitGameState() {
+	width, height := screen.Size()
+	paddleStart := height/2 - PaddleHeight/2
+
+	player1Paddle = &GameObject{
+		row: paddleStart, col: 0, width: 1, height: PaddleHeight,
+		velRow: 0, velCol: 0,
+		symbol: PaddleSymbol,
+	}
+
+	player2Paddle = &GameObject{
+		row: paddleStart, col: width - 1, width: 1, height: PaddleHeight,
+		velRow: 0, velCol: 0,
+		symbol: PaddleSymbol,
+	}
+
+	ball = &GameObject{
+		row: height / 2, col: width / 2, width: 1, height: 1,
+		velRow: InitialBallVelocityRow, velCol: InitialBallVelocityCol,
+		symbol: BallSymbol,
+	}
+
+	gameObjects = []*GameObject{
+		player1Paddle, player2Paddle, ball,
+	}
+}
+
+func HandleUserInput(key string) {
+	_, screenHeight := screen.Size()
+	if key == "Rune[q]" {
+		screen.Fini()
+		os.Exit(0)
+	} else if key == "Rune[w]" && player1Paddle.row > 0 {
+		player1Paddle.row--
+	} else if key == "Rune[s]" && player1Paddle.row+player1Paddle.height < screenHeight {
+		player1Paddle.row++
+	} else if key == "Up" && player2Paddle.row > 0 {
+		player2Paddle.row--
+	} else if key == "Down" && player2Paddle.row+player2Paddle.height < screenHeight {
+		player2Paddle.row++
+	} else if key == "Rune[p]" {
+		isGamePaused = !isGamePaused
+	}
+}
+
+func InitUserInput() chan string {
+	inputChan := make(chan string)
+	go func() {
+		for {
+			switch ev := screen.PollEvent().(type) {
+			case *tcell.EventKey:
+				inputChan <- ev.Name()
+			}
+		}
+	}()
+
+	return inputChan
+}
+
+func ReadInput(inputChan chan string) string {
+	var key string
+	select {
+	case key = <-inputChan:
+	default:
+		key = ""
+	}
+
+	return key
+}
+
+func PrintStringCentered(row, col int, str string) {
+	col = col - len(str)/2
+	PrintString(row, col, str)
+}
+
+func PrintString(row, col int, str string) {
+	for _, c := range str {
+		screen.SetContent(col, row, c, nil, tcell.StyleDefault)
+		col += 1
+	}
+}
+
+func Print(row, col, width, height int, ch rune) {
+	for r := 0; r < height; r++ {
+		for c := 0; c < width; c++ {
+			screen.SetContent(col+c, row+r, ch, nil, tcell.StyleDefault)
+		}
+	}
+}
