4812cbb781ca757bef8dbbe6e014c4a3e17e6eec
Preslav Mihaylov <preslav@kingslanduniversity.com>
2021-07-14 09:07:58 +0300

    change solution after rerecording

--- 04_control-flow-structures/examples/09_point-in-rectangle/main.go
+++ 04_control-flow-structures/examples/09_point-in-rectangle/main.go
@@ -4,16 +4,13 @@ import "fmt"
 
 func main() {
 	// Check if the point is inside the rectangle
-	pointX, pointY := 6, 3
+	pointX, pointY := 3, 3
 	topLeftX, topLeftY := 1, 5
 	botRightX, botRightY := 5, 1
 
-	aboveBotLine := botRightY < pointY
-	belowTopLine := topLeftY > pointY
-	leftOfRightLine := botRightX > pointX
-	rightOfLeftLine := topLeftX < pointX
-
-	if aboveBotLine && belowTopLine && leftOfRightLine && rightOfLeftLine {
+	betweenLeftAndRight := pointX > topLeftX && pointX < botRightX
+	betweenBotAndTop := pointY > botRightY && pointY < topLeftY
+	if betweenLeftAndRight && betweenBotAndTop {
 		fmt.Println("The point is inside the rectangle")
 	} else {
 		fmt.Println("The point is outside the rectangle")
