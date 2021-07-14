89745d5b0e536c758807c460451f516ba46675bc
Preslav Mihaylov <preslav@kingslanduniversity.com>
2021-07-14 19:17:44 +0300

    add shorthand operators example

---
+++ 02_data-types-and-variables/examples/15_shorthand_operations/go.mod
@@ -0,0 +1,3 @@
+module 15_shorthand_operations
+
+go 1.16
---
+++ 02_data-types-and-variables/examples/15_shorthand_operations/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b, c, d int
+
+	// Longer version: a = a + 5
+	a += 5
+
+	// Longer version: b = b - 3
+	b -= 3
+
+	// Longer version: c = c * 2
+	c *= 2
+
+	// Longer version: d = d / 2
+	d /= 2
+
+	fmt.Println(a, b, c, d)
+}
